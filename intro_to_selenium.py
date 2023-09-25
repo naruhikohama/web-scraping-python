17c3a34f23cbac93a5d10bf13b293e4d5320bfa1
naruhikohama <naruhikohama@gmail.com>
2023-09-24 23:44:12 -0300

    adjusting selenium version

--- intro_to_selenium.py
+++ intro_to_selenium.py
@@ -1,6 +1,6 @@
 from selenium import webdriver 
 
 website = 'https://www.adamchoi.co.uk/overs/detailed'  
-path = 'C:/Users/Naruhiko/Downloads/chromedriver_win32'
+path = 'C:/Users/Naruhiko/Downloads/chromedriver_win32/chromedriver.exe'
 driver = webdriver.Chrome(path)
 driver.get(website)
