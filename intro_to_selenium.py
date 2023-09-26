3ff11255276a25bbebc0421a3a11ee287f5932ce
naruhikohama <naruhikohama@gmail.com>
2023-09-26 20:48:39 -0300

    installing new chromedriver and adjusting script to keep page opened

--- intro_to_selenium.py
+++ intro_to_selenium.py
@@ -1,6 +1,13 @@
 from selenium import webdriver 
+from selenium.webdriver.chrome.options import Options
+
+def launchBrowser(website, webdriver_path):
+    chr_options = Options()
+    chr_options.add_experimental_option("detach", True)
+    driver = webdriver.Chrome(webdriver_path, options=chr_options)
+    driver.get(website)
 
 website = 'https://www.adamchoi.co.uk/overs/detailed'  
-path = 'C:/Users/Naruhiko/Downloads/chromedriver_win32/chromedriver.exe'
-driver = webdriver.Chrome(path)
-driver.get(website)
+path = 'C:/Users/Naruhiko/Downloads/chromedriver/chromedriver-win32/chromedriver.exe'
+
+launchBrowser(website = website, webdriver_path = path)
