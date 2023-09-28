b1edc3a4f478ca20c3cab8783296f3640b6bfc94
naruhikohama <naruhikohama@gmail.com>
2023-09-27 23:40:11 -0300

    Clicking on a button with selenium and xpath

--- intro_to_selenium.py
+++ intro_to_selenium.py
@@ -1,11 +1,17 @@
 from selenium import webdriver 
-from selenium.webdriver.chrome.options import Options
+# from selenium.webdriver.chrome.options import Options
+from time import sleep
 
 def launchBrowser(website, webdriver_path):
-    chr_options = Options()
-    chr_options.add_experimental_option("detach", True)
-    driver = webdriver.Chrome(webdriver_path, options=chr_options)
+    driver = webdriver.Chrome(webdriver_path)
     driver.get(website)
+
+    all_matches_button = driver.find_element('xpath', '//label[@analytics-event="All matches"]')
+    all_matches_button.click()
+
+    while True:
+        sleep(5)
+        pass
 
 website = 'https://www.adamchoi.co.uk/overs/detailed'  
 path = 'C:/Users/Naruhiko/Downloads/chromedriver/chromedriver-win32/chromedriver.exe'
