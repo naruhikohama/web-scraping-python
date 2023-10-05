c31b0d7fed1bdf7a2ad5321db8c85aa67fa594e9
naruhikohama <naruhikohama@gmail.com>
2023-10-05 20:43:43 -0300

    scraping single page - now working correctly

--- scraping_mulitple_pages_with_selenium.py
+++ scraping_mulitple_pages_with_selenium.py
@@ -1,15 +1,22 @@
 from selenium import webdriver 
 import pandas as pd
+from time import sleep
 
 website = 'https://www.audible.com/search'
-path = 'C:/Users/Naruhiko/Downloads/chromedriver/chromedriver-win32/chromedriver.exe'    
-driver = webdriver.Chrome(path)
+path = 'C:/Users/Naruhiko/Downloads/chromedriver/chromedriver-win32/chromedriver.exe'   
+options = webdriver.ChromeOptions()
+# options.add_argument("--ignore-certificate-error")
+# options.add_argument("--ignore-ssl-errors") 
+# user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/33.0.1750.517 Safari/537.36'
+# options.add_argument('user-agent={0}'.format(user_agent))
+driver = webdriver.Chrome(path, options=options)
 driver.get(website) 
 
 driver.maximize_window() 
+sleep(2)
 
 container = driver.find_element('xpath', '//div[@class="adbl-impression-container "]')
-products = container.find_elements('xpath', './li')
+products = container.find_elements('xpath', './/li[contains(@class, "productListItem")]')
 
 book_title = []
 book_author = []
@@ -22,17 +29,11 @@ for product in products:
     print(title, author, runtime)
 
     book_title.append(title)
-    book_author.append(author)  
-    book_length.append(runtime)
+    book_author.append(author.split(':')[1].strip())  
+    book_length.append(runtime.split(':')[1].strip())
 
 driver.quit()
 
 df_books = pd.DataFrame({'title': book_title, 'author': book_author, 'length': book_length})
 df_books.to_csv('books.csv', index = False)
 
-
-# .//li[contains(@class, "productListItem")]
-
-# This means that the "products" variable will be:
-
-# products = container.find_elements_by_xpath('.//li[contains(@class, "productListItem")]')
