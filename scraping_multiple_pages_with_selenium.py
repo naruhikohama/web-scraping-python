416bc956f2ffdbba4b2cf3ff7fbfa5da1286df32
naruhikohama <naruhikohama@gmail.com>
2023-10-06 20:51:55 -0300

    Scraping in headless mode

--- scraping_multiple_pages_with_selenium.py
+++ scraping_multiple_pages_with_selenium.py
@@ -1,19 +1,20 @@
 from selenium import webdriver 
+from selenium.webdriver.chrome.options import Options
 import pandas as pd
 from time import sleep
 
 website = 'https://www.audible.com/search'
 path = 'C:/Users/Naruhiko/Downloads/chromedriver/chromedriver-win32/chromedriver.exe'   
-options = webdriver.ChromeOptions()
-# options.add_argument("--ignore-certificate-error")
-# options.add_argument("--ignore-ssl-errors") 
-# user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/33.0.1750.517 Safari/537.36'
-# options.add_argument('user-agent={0}'.format(user_agent))
+options = Options()
+options.headless = True # run in the background
+options.add_argument("--window-size=1920,1080")
+user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36'
+options.add_argument(f'user-agent={user_agent}')
 driver = webdriver.Chrome(path, options=options)
 driver.get(website) 
 
-driver.maximize_window() 
-sleep(2)
+# driver.maximize_window() 
+# sleep(2)
 
 container = driver.find_element('xpath', '//div[@class="adbl-impression-container "]')
 products = container.find_elements('xpath', './/li[contains(@class, "productListItem")]')
