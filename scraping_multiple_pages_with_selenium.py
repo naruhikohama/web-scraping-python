c51234a2571bc3e1e2c73101a4980b4e2ad91ade
naruhikohama <naruhikohama@gmail.com>
2023-10-06 23:29:43 -0300

    scraping multiple pages with selenium

--- scraping_multiple_pages_with_selenium.py
+++ scraping_multiple_pages_with_selenium.py
@@ -6,32 +6,46 @@ from time import sleep
 website = 'https://www.audible.com/search'
 path = 'C:/Users/Naruhiko/Downloads/chromedriver/chromedriver-win32/chromedriver.exe'   
 options = Options()
-options.headless = True # run in the background
+options.add_argument("--headless=new")
 options.add_argument("--window-size=1920,1080")
 user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36'
 options.add_argument(f'user-agent={user_agent}')
 driver = webdriver.Chrome(path, options=options)
 driver.get(website) 
 
-# driver.maximize_window() 
-# sleep(2)
-
 container = driver.find_element('xpath', '//div[@class="adbl-impression-container "]')
 products = container.find_elements('xpath', './/li[contains(@class, "productListItem")]')
+
+pagination = driver.find_element('xpath', '//ul[contains(@class, "pagingElements")]')
+pages = pagination.find_elements('xpath', './li')
+last_page = int(pages[-2].text) # -1 is next button, -2 is last page
+
+current_page = 1 
 
 book_title = []
 book_author = []
 book_length = []
+while current_page <= last_page:
+    print(current_page)
+    for product in products:
+        title = product.find_element('xpath', './/h3[contains(@class, "bc-heading")]').text
+        author = product.find_element('xpath', './/li[contains(@class, "authorLabel")]').text
+        runtime = product.find_element('xpath', './/li[contains(@class, "runtimeLabel")]').text
+        # print(title, author, runtime)
 
-for product in products:
-    title = product.find_element('xpath', './/h3[contains(@class, "bc-heading")]').text
-    author = product.find_element('xpath', './/li[contains(@class, "authorLabel")]').text
-    runtime = product.find_element('xpath', './/li[contains(@class, "runtimeLabel")]').text
-    print(title, author, runtime)
+        book_title.append(title)
+        book_author.append(author.split(':')[1].strip())  
+        book_length.append(runtime.split(':')[1].strip())
+    
+    current_page += 1
 
-    book_title.append(title)
-    book_author.append(author.split(':')[1].strip())  
-    book_length.append(runtime.split(':')[1].strip())
+    try:
+        next_page = driver.find_element('xpath', '//li[contains(@class, "nextButton")]') 
+        next_page.click()
+
+    except:
+        print('No more pages to load.')
+        pass
 
 driver.quit()
 
