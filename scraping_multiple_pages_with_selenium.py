1da08d05fdbaf0a81ddde7176054308942d6c3f6
naruhikohama <naruhikohama@gmail.com>
2023-10-07 22:24:12 -0300

    fixing code and adding explicit waits

--- scraping_multiple_pages_with_selenium.py
+++ scraping_multiple_pages_with_selenium.py
@@ -1,20 +1,20 @@
 from selenium import webdriver 
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait 
+from selenium.webdriver.support import expected_conditions as EC
 import pandas as pd
 from time import sleep
 
 website = 'https://www.audible.com/search'
 path = 'C:/Users/Naruhiko/Downloads/chromedriver/chromedriver-win32/chromedriver.exe'   
 options = Options()
-options.add_argument("--headless=new")
+# options.add_argument("--headless=new")
 options.add_argument("--window-size=1920,1080")
 user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36'
 options.add_argument(f'user-agent={user_agent}')
 driver = webdriver.Chrome(path, options=options)
 driver.get(website) 
-
-container = driver.find_element('xpath', '//div[@class="adbl-impression-container "]')
-products = container.find_elements('xpath', './/li[contains(@class, "productListItem")]')
 
 pagination = driver.find_element('xpath', '//ul[contains(@class, "pagingElements")]')
 pages = pagination.find_elements('xpath', './li')
@@ -26,6 +26,11 @@ book_title = []
 book_author = []
 book_length = []
 while current_page <= last_page:
+    # sleep(1)
+    # container = driver.find_element('xpath', '//div[@class="adbl-impression-container "]')
+    # products = container.find_elements('xpath', './/li[contains(@class, "productListItem")]')
+    container = WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.XPATH, '//div[@class="adbl-impression-container "]')))
+    products = WebDriverWait(container, 5).until(EC.presence_of_all_elements_located((By.XPATH, './/li[contains(@class, "productListItem")]')))
     print(current_page)
     for product in products:
         title = product.find_element('xpath', './/h3[contains(@class, "bc-heading")]').text
@@ -40,7 +45,7 @@ while current_page <= last_page:
     current_page += 1
 
     try:
-        next_page = driver.find_element('xpath', '//li[contains(@class, "nextButton")]') 
+        next_page = driver.find_element('xpath', '//span[contains(@class, "nextButton")]') 
         next_page.click()
 
     except:
