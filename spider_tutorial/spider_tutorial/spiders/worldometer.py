b206400497124930b38ab8d10ce1277ce33a99af
naruhikohama <naruhikohama@gmail.com>
2023-10-19 20:11:35 -0300

    scraping with scrapy

--- spider_tutorial/spider_tutorial/spiders/worldometer.py
+++ spider_tutorial/spider_tutorial/spiders/worldometer.py
@@ -7,4 +7,14 @@ class WorldometerSpider(scrapy.Spider):
     start_urls = ["https://www.worldometers.info/world-population/population-by-country"]
 
     def parse(self, response):
-        pass
+        # title = response.xpath("//h1/text()").get() 
+        countries = response.xpath("//td/a")
+
+        for country in countries:
+            country_name = country.xpath(".//text()").get()
+            link = country.xpath(".//@href").get()
+
+            yield {
+                "country_name": country_name,
+                "link": link
+            }
