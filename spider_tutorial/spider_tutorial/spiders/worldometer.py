820865e215be5537f5358e124f48ba2d29379fc8
naruhikohama <naruhikohama@gmail.com>
2023-10-20 22:44:15 -0300

    scraping multiple pages with scrapy

--- spider_tutorial/spider_tutorial/spiders/worldometer.py
+++ spider_tutorial/spider_tutorial/spiders/worldometer.py
@@ -14,7 +14,21 @@ class WorldometerSpider(scrapy.Spider):
             country_name = country.xpath(".//text()").get()
             link = country.xpath(".//@href").get()
 
+            # retrieve data with an absolute url
+            # absolute_url = response.urljoin(link)
+            # yield scrapy.Request(url=absolute_url)
+
+            # retrieve data with a relative url
+            yield response.follow(url=link, callback=self.parse_country, meta={'country_name': country_name})
+    
+    def parse_country(self, response):
+        rows = response.xpath('(//table[contains(@class, "table")])[1]/tbody/tr')
+        for row in rows:
+            year = row.xpath(".//td[1]/text()").get()
+            population = row.xpath(".//td[2]/strong/text()").get()
+
             yield {
-                "country_name": country_name,
-                "link": link
+                'country': response.request.meta['country_name'],
+                'year': year,
+                'population': population,
             }
