c6560d4743674478fefa4a8c812822d6ea2ede74
naruhikohama <naruhikohama@gmail.com>
2023-10-26 22:06:57 -0300

    configuring spider to scrape audible

--- spider_tutorial/spider_tutorial/spiders/audible.py
+++ spider_tutorial/spider_tutorial/spiders/audible.py
@@ -7,4 +7,17 @@ class AudibleSpider(scrapy.Spider):
     start_urls = ["https://www.audible.com/search"]
 
     def parse(self, response):
-        pass
+        product_container = response.xpath('//div[@class="adbl-impression-container "]//li[contains(@class, "productListItem")]')
+
+        for product in product_container:
+            title = product.xpath('.//h3[contains(@class, "bc-heading")]/a/text()').get()
+            author = product.xpath('.//li[contains(@class, "authorLabel")]/span/a/text()').get()
+            runtime = product.xpath('.//li[contains(@class, "runtimeLabel")]/span/text()').get().split(':')[1].strip()
+
+            print(title, author, runtime)
+
+            yield {
+                'title': title,
+                'author': author,
+                'length': runtime
+            }
