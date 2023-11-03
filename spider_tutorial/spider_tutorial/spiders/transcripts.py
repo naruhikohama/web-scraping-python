3661c894aa5a7fcd71d83308df31f41724f7b16b
naruhikohama <naruhikohama@gmail.com>
2023-11-02 23:11:17 -0300

    crawling through many pages with scrapy

--- spider_tutorial/spider_tutorial/spiders/transcripts.py
+++ spider_tutorial/spider_tutorial/spiders/transcripts.py
@@ -6,15 +6,17 @@ from scrapy.spiders import CrawlSpider, Rule
 class TranscriptsSpider(CrawlSpider):
     name = "transcripts"
     allowed_domains = ["subslikescript.com"]
-    start_urls = ["https://subslikescript.com/movies"]
+    start_urls = ["https://subslikescript.com/movies_letter-U"]
 
-    rules = (Rule(LinkExtractor(restrict_xpaths="//ul[@class='scripts-list']/a"), callback="parse_item", follow=True),)
+    rules = (Rule(LinkExtractor(restrict_xpaths="//ul[@class='scripts-list']/a"), callback="parse_item", follow=True),
+             Rule(LinkExtractor(restrict_xpaths="(//a[@rel='next'])[1]"))
+             )
 
     def parse_item(self, response):
         article = response.xpath("//article[@class='main-article']")
         yield {
             'title': article.xpath(".//h1/text()").get(),
             'plot':article.xpath(".//p/text()").get(),
-            'transcript': article.xpath(".//div[@class='full-script']/text()").getall(),
+            # 'transcript': article.xpath(".//div[@class='full-script']/text()").getall(),
             'url': response.url
         }
