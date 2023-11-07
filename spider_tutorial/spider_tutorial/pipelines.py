e2e228509c3f52cafcb03692a05a032be6473f66
naruhikohama <naruhikohama@gmail.com>
2023-11-06 23:21:46 -0300

    saving data to a sql database - sqlite

--- spider_tutorial/spider_tutorial/pipelines.py
+++ spider_tutorial/spider_tutorial/pipelines.py
@@ -8,6 +8,7 @@
 from itemadapter import ItemAdapter
 import logging
 import pymongo
+import sqlite3
 
 
 class MongodbPipeline:
@@ -23,3 +24,35 @@ class MongodbPipeline:
     def process_item(self, item, spider):
         self.db[self.collection_name].insert_one(item)
         return item
+    
+class SqlitePipeline:
+    def open_spider(self, spider):
+        self.connection = sqlite3.connect("transcripts.db")
+        self.c = self.connection.cursor()
+        try:
+            self.c.execute("""
+                           CREATE TABLE transcripts(
+                                title TEXT,
+                                plot TEXT,
+                                transcript TEXT,
+                                url TEXT
+                            )
+                           """
+                           )
+            self.connection.commit()
+        except sqlite3.OperationalError:
+            pass
+
+    def close_spider(self, spider):
+        self.connection.close()
+
+    
+    def process_item(self, item, spider):
+        self.c.execute("INSERT INTO transcripts VALUES (?, ?, ?, ?)", 
+                       (item.get('title'),
+                       item.get('plot'),
+                       item.get('transcript'),
+                       item.get('url')))
+        self.connection.commit()
+        return item
+
--- spider_tutorial/spider_tutorial/spiders/transcripts.py
+++ spider_tutorial/spider_tutorial/spiders/transcripts.py
@@ -23,9 +23,11 @@ class TranscriptsSpider(CrawlSpider):
 
     def parse_item(self, response):
         article = response.xpath("//article[@class='main-article']")
+        transcript_list = article.xpath(".//div[@class='full-script']/text()")
+        transcript_string = ' '.join(transcript_list.getall())
         yield {
             'title': article.xpath(".//h1/text()").get(),
             'plot':article.xpath(".//p/text()").get(),
-            # 'transcript': article.xpath(".//div[@class='full-script']/text()").getall(),
+            'transcript': transcript_string,
             'url': response.url
         }
