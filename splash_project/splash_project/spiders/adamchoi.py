6a6bb1b89adb18a691706b6689ddfe5e4def7713
naruhikohama <naruhikohama@gmail.com>
2023-11-24 23:48:30 -0300

    adding lua code to scrapy

--- splash_project/splash_project/spiders/adamchoi.py
+++ splash_project/splash_project/spiders/adamchoi.py
@@ -6,5 +6,22 @@ class AdamchoiSpider(scrapy.Spider):
     allowed_domains = ["www.adamchoi.co.uk"]
     start_urls = ["https://www.adamchoi.co.uk/"]
 
+    script = """
+    function main(splash, args)
+    splash.private_mode_enabled = false
+      assert(splash:go(args.url))
+      assert(splash:wait(3))
+      all_matches = assert(splash:select_all('label.btn.btn-sm.btn-primary'))
+      all_matches[2]:mouse_click()
+      assert(splash:wait(3))
+      splash:set_viewport_full()
+      return {
+        html = splash:html(),
+        png = splash:png()
+      }
+    end
+
+    """
+
     def parse(self, response):
         pass
