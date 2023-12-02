0b4e965588a884aa30900912dcdb65d81b2730d1
naruhikohama <naruhikohama@gmail.com>
2023-12-01 22:29:49 -0300

    scraping with splash and scrapy

--- splash_project/splash_project/spiders/adamchoi.py
+++ splash_project/splash_project/spiders/adamchoi.py
@@ -1,5 +1,5 @@
 import scrapy
-
+from scrapy_splash import SplashRequest
 
 class AdamchoiSpider(scrapy.Spider):
     name = "adamchoi"
@@ -10,10 +10,10 @@ class AdamchoiSpider(scrapy.Spider):
     function main(splash, args)
     splash.private_mode_enabled = false
       assert(splash:go(args.url))
-      assert(splash:wait(3))
+      assert(splash:wait(1))
       all_matches = assert(splash:select_all('label.btn.btn-sm.btn-primary'))
       all_matches[2]:mouse_click()
-      assert(splash:wait(3))
+      assert(splash:wait(1))
       splash:set_viewport_full()
       return {
         html = splash:html(),
@@ -23,5 +23,16 @@ class AdamchoiSpider(scrapy.Spider):
 
     """
 
+    def start_requests(self):
+        yield SplashRequest(
+            url="https://www.adamchoi.co.uk/overs/detailed",
+            callback=self.parse,
+            endpoint="execute",
+            args={
+                "lua_source": self.script,
+                "wait": 1,
+            },
+        )
+
     def parse(self, response):
-        pass
+        print(response.body)
